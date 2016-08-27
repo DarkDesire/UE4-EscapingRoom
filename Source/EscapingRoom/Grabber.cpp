3207d39ec6172a91b648584717b365f85e2279df
DarkDesire <oh.eldarkaa@gmail.com>
2016-08-27 22:39:18 +0300

    Added MyEscapingRoom blueprint inherits from EscapingRoomGame_c Added also MyStarterPawn blueprint inherits from DefaultPawn Added ActorComponent C++ (Grabber.c) to new MyStarterPawn Got PlayerViewport in Grabber.c

---
+++ Source/EscapingRoom/Grabber.cpp
@@ -0,0 +1,52 @@
+// Copyright Eldar Nezametdinov 2016
+
+#include "EscapingRoom.h"
+#include "Grabber.h"
+
+#define OUT
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+  FString ObjectName = GetOwner()->GetName();
+  UE_LOG( LogTemp, Warning, TEXT( "Grabber is working in %s" ), *ObjectName );
+	
+  PlayerController = GetWorld()->GetFirstPlayerController();
+
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+  // Get player view point this tick
+  FVector PlayerViewPointLocation;
+  FRotator PlayerViewPointRotation;
+  PlayerController->GetPlayerViewPoint(
+    OUT PlayerViewPointLocation,
+    OUT PlayerViewPointRotation );
+  // Debug printing
+  UE_LOG( LogTemp, Warning, TEXT( " Location is: %s , rotation: %s" ), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString() );
+
+  // Ray-cast out to reach distance 
+
+
+
+}
+
---
+++ Source/EscapingRoom/Grabber.h
@@ -0,0 +1,30 @@
+// Copyright Eldar Nezametdinov 2016
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPINGROOM_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+private:
+  APlayerController* PlayerController;
+  
+  
+};
