ed6521a2c32e543582cfa448b1b249d5c8342ca4
DarkDesire <oh.eldarkaa@gmail.com>
2016-08-27 03:31:54 +0300

    init

---
+++ Source/EscapingRoom/EscapingRoomGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EscapingRoomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPINGROOM_API AEscapingRoomGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
