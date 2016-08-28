c6b57fdddb0f6ec1903c54e9b416bd20f7faf12a
DarkDesire <oh.eldarkaa@gmail.com>
2016-08-28 08:38:51 +0300

    Exposing Events to Blueprint

--- Source/EscapingRoom/OpenDoor.cpp
+++ Source/EscapingRoom/OpenDoor.cpp
@@ -28,7 +28,8 @@ void UOpenDoor::BeginPlay()
 
 void UOpenDoor::OpenDoor()
 {
-  GetOwner()->SetActorRotation( FRotator( 0.f, OpenAngle, 0.f ) );
+  //GetOwner()->SetActorRotation( FRotator( 0.f, OpenAngle, 0.f ) );
+  OnOpenRequest.Broadcast();
 }
 
 void UOpenDoor::CloseDoor()
